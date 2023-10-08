9dad77453f6cbe138ac107a174cafceb4eebfed8
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-10-08 12:07:16 -0500

    Add files via upload

---
+++ venv/htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://pbs.twimg.com/profile_images/1592269659201753088/mxsCuC8T_400x400.jpg">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://www.svgrepo.com/show/382111/female-avatar-girl-face-woman-user-5.svg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
